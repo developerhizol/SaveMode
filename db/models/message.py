c1d500bb20b98b85cb0ebac7984c1483cdb71123
developerhizol <kostatrofimcev11@gmail.com>
2026-05-18 07:29:14 +0300

    Delete db/models/message.py

--- db/models/message.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import SQLModel, Field
-
-
-class Message(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    chat_id: int
-    content: str
-    type: str
-    from_username: str
